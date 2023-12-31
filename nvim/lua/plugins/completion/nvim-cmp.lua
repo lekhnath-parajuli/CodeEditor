8f8d8d65a33eda60d8f5ba1c9ac00fde59a0b83a
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2023-12-31 16:27:05 +0545

    working on command line auto complete

--- nvim/lua/plugins/completion/nvim-cmp.lua
+++ nvim/lua/plugins/completion/nvim-cmp.lua
@@ -1,18 +1,49 @@
 return {
   "hrsh7th/nvim-cmp",
-  event="InsertEnter",
+  event={"InsertEnter"},
   dependencies={
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/nvim-cmp"
+    { "hrsh7th/cmp-cmdline", event={"CmdLineEnter"}},
+    { "neovim/nvim-lspconfig", event={"InsertEnter"}},
+    { "hrsh7th/cmp-nvim-lsp", event={"InsertEnter"}},
+    { "hrsh7th/cmp-buffer", event={"InsertEnter"}},
+    { "hrsh7th/cmp-path", event={"InsertEnter"}},
+    { "hrsh7th/nvim-cmp", event={"InsertEnter"}},
   },
+
   config=function()
-    require("cmp").setup({
+    local cmp = require "cmp"
+
+    local generalSources = {
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+      { name = "buffer" },
+      { name = "path" },
+    }
+
+    local searchSources = {
+      mapping = cmp.mapping.preset.cmdline(),
       sources = {
-        { name = "buffer" }
+        { name = 'buffer' }
       }
-    })
+    }
+
+    local pathSources = {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        {
+          name = 'cmdline',
+          option = {
+            ignore_cmds = { 'Man', '!' }
+          }
+        }
+      })
+    }
+
+    cmp.setup({sources = cmp.config.sources(generalSources)})
+    cmp.setup.cmdline('/', searchSources)
+    cmp.setup.cmdline(':', pathSources)
   end
 }
 
