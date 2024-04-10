0551d7d42f111790b2bb7190462ba50cfd85375d
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2024-04-10 16:26:43 +0545

    fix setup script for ubuntu

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+apt remove neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage
 ./nvim/setup.sh
 
+rm nvim.appimage
 # rm -rf ~/.config/nvim
 # git clone https://github.com/LazyVim/starter ~/.config/nvim
