9aa1926dc5c3543392d87bcfe0be5700401a9fe3
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2024-04-10 16:35:05 +0545

    all commands are not running fix

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-apt remove neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage
-./nvim/setup.sh
-
-rm nvim.appimage
+apt remove neovim \
+  && curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage \
+  && chmod u+x nvim.appimage \
+  && ./nvim.appimage \
+  && ./nvim/setup.sh \
+  && rm nvim.appimage
 # rm -rf ~/.config/nvim
 # git clone https://github.com/LazyVim/starter ~/.config/nvim
